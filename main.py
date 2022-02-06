119715c7a5d4d4f0eed10565adb8357e0fb42ed6
Niikurasu <39441663+Niikurasu@users.noreply.github.com>
2022-02-06 14:54:42 +0000

    Adding color

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
+#!/usr/bin/env python
+# coding: utf8
 
 """
 Codingame Python Code Shortener
 Version - 1
 Niikurasu :)
-
-if you encounter errors try 'export PYTHONIOENCODING=utf8' in the shell
 """
 
-
-
 import python_minifier
+from colorama import Fore, Back, Style
+
+# print out 
+
+
+
+# read code from text file
 with open("code.txt") as file:
-  lines = file.readlines()
+    lines = file.readlines()
 SOURCE_CODE = "".join(i for i in lines)
-original_length = len(SOURCE_CODE)
-print("Starting length:", original_length)
+
+# minify code
 minified = python_minifier.minify(SOURCE_CODE)
+
+# check code sizes
+original_length = len(SOURCE_CODE)
 minified_length = len(minified)
-print("Minified length:", minified_length)
-# use minified code if it shortened it
+
+
+# use minified code if it's shorter
 if minified_length < original_length:
-  SOURCE_CODE = minified
-# convert code to utf-16 if it shortens it
-print(SOURCE_CODE)
-t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
+    SOURCE_CODE = minified
+
+# convert code to utf-16
+try:
+    t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
+except:
+  index = SOURCE_CODE.index(')')
+  SOURCE_CODE = SOURCE_CODE[:index] + ' ' + SOURCE_CODE[index:]
+  t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
 u16_code = f"exec(bytes('{t}','u16')[2:])"
 u16_length = len(u16_code)
-print("UTF-16 length:", u16_length)
+# change code if it's shorter
 if u16_length < len(SOURCE_CODE):
-  SOURCE_CODE = u16_code
-print('\n\n\n' + SOURCE_CODE)
+    SOURCE_CODE = u16_code
+
+print("Starting length:", original_length)
+print("Minified length:", minified_length)
+print("UTF-16 length:", u16_length)
+
+# print result
+print('\nShortest Code:')
+
+print(SOURCE_CODE)
