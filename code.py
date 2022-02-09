7ed7e704b7555280427a5dd0f64c5a7fa441ad79
Niikurasu <e.niklas@protonmail.com>
2022-02-09 19:46:01 +0100

    cli interfacae

--- code.py
+++ code.py
@@ -1,2 +1,5 @@
-import math
-print(int(math.log2(int(input())))+1)
+string = input().lower().replace(" ", "")
+x = ""
+for i in string:
+    if i not in x:x += i
+print(" ".join(f"{i}:{string.count(i)}" for i in x))
--- main.py
+++ main.py
@@ -8,46 +8,62 @@
 """
 
 import python_minifier
+import click
 from colorama import Fore, Style, init
 
-# read code from text file
-with open("code.py") as file:
-    lines = file.readlines()
-SOURCE_CODE = "".join(i for i in lines)
 
-# minify code
-minified = python_minifier.minify(SOURCE_CODE)
-
-# check code sizes
-original_length = len(SOURCE_CODE)
-minified_length = len(minified)
+# cli interface
+@click.command()
+@click.option('f', '--file', required=True, help='the file path of the Python script that gets shortened')
 
 
-# use minified code if it's shorter
-if minified_length < original_length:
-    SOURCE_CODE = minified
 
-# convert code to utf-16
+def shorten_code():
+    """
+    shortens code 
+    """
+    # read code from text file
+    with open("code.py") as file:
+        lines = file.readlines()
+    SOURCE_CODE = "".join(i for i in lines)
 
-try:
+    # minify code
+    minified = python_minifier.minify(SOURCE_CODE)
+
+    # check code sizes
+    original_length = len(SOURCE_CODE)
+    minified_length = len(minified)
+
+
+    # use minified code if it's shorter
+    if minified_length < original_length:
+        SOURCE_CODE = minified
+
+    # convert code to utf-16
+
+    try:
+        t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
+    except:
+    index = SOURCE_CODE.index(')')
+    SOURCE_CODE = SOURCE_CODE[:index] + ' ' + SOURCE_CODE[index:]
     t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
-except:
-  index = SOURCE_CODE.index(')')
-  SOURCE_CODE = SOURCE_CODE[:index] + ' ' + SOURCE_CODE[index:]
-  t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
-u16_code = f"exec(bytes('{t}','u16')[2:])"
-u16_length = len(u16_code)
-# change code if it's shorter
-if u16_length < len(SOURCE_CODE):
-    SOURCE_CODE = u16_code
-# print out different code lengths
-init()
-print(Fore.RED + Style.BRIGHT + "Starting length:" + Style.RESET_ALL, original_length)
-print(Fore.RED + Style.BRIGHT + "Minified length:"+ Style.RESET_ALL, minified_length)
-print(Fore.RED + Style.BRIGHT + "UTF-16 length:" + Style.RESET_ALL, u16_length)
+    u16_code = f"exec(bytes('{t}','u16')[2:])"
+    u16_length = len(u16_code)
+    # change code if it's shorter
+    if u16_length < len(SOURCE_CODE):
+        SOURCE_CODE = u16_code
+    # print out different code lengths
+    init()
+    print(Fore.RED + Style.BRIGHT + "Starting length:" + Style.RESET_ALL, original_length)
+    print(Fore.RED + Style.BRIGHT + "Minified length:"+ Style.RESET_ALL, minified_length)
+    print(Fore.RED + Style.BRIGHT + "UTF-16 length:" + Style.RESET_ALL, u16_length)
 
 
-# print result
-print(Style.RESET_ALL + 'Shortest Code:\n')
+    # print result
+    print(Style.RESET_ALL + 'Shortest Code:\n')
 
-print(SOURCE_CODE)
+    print(SOURCE_CODE)
+
+if __name__ == '__main__':
+    print_title()
+    shorten_code()
