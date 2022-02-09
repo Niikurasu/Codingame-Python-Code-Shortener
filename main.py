c26add6be8baa22668089859a0e9d1f99fc8e9ef
Niikurasu <e.niklas@protonmail.com>
2022-02-09 20:02:00 +0100

    command interface + cat

--- main.py
+++ main.py
@@ -17,14 +17,21 @@ from colorama import Fore, Style, init
 @click.option('f', '--file', required=True, help='the file path of the Python script that gets shortened')
 
 
-
-def shorten_code():
-    """
-    shortens code 
-    """
+def main(f):
+    # 
+    print(Fore.GREEN + Style.BRIGHT + """
+      /\_____/\\
+     /  o   o  \\        CODINGAME  Python CODE SHORTENER
+    ( ==  ^  == )       Version: 1.0
+     )         (         Author: Niikurasu
+    (           )
+   ( (  )   (  ) )
+  (__(__)___(__)__)
+        """)
+    
     # read code from text file
-    with open("code.py") as file:
-        lines = file.readlines()
+    with open(f) as codefile:
+        lines = codefile.readlines()
     SOURCE_CODE = "".join(i for i in lines)
 
     # minify code
@@ -40,13 +47,12 @@ def shorten_code():
         SOURCE_CODE = minified
 
     # convert code to utf-16
-
     try:
         t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
     except:
-    index = SOURCE_CODE.index(')')
-    SOURCE_CODE = SOURCE_CODE[:index] + ' ' + SOURCE_CODE[index:]
-    t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
+        index = SOURCE_CODE.index(')')
+        SOURCE_CODE = SOURCE_CODE[:index] + ' ' + SOURCE_CODE[index:]
+        t = bytes(SOURCE_CODE, 'utf8').decode('utf16')
     u16_code = f"exec(bytes('{t}','u16')[2:])"
     u16_length = len(u16_code)
     # change code if it's shorter
@@ -60,10 +66,10 @@ def shorten_code():
 
 
     # print result
-    print(Style.RESET_ALL + 'Shortest Code:\n')
-
+    print(Style.RESET_ALL + '\nShortest Code:')
     print(SOURCE_CODE)
 
+
+
 if __name__ == '__main__':
-    print_title()
-    shorten_code()
+    main()
